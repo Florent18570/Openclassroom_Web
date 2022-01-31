4278d6e98e540c5a829aa704f45e276f95760070
Florent18570 <florent.escots@gmail.com>
2022-01-31 14:11:14 +0100

    page_accueil

---
+++ Page_accueil.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<header>
+    <title> Page d'accueil</title>
+    <?php include('bdd.php'); ?>
+</header>
+<body>
+
+
+    <H1 style="text-align: center;"> 
+        Fiche de révion cahier Anglais
+    </H1>
+
+    <h2>
+        Ajout d'une page de révision
+    </h2>
+
+    <form method="post" action="tempon.php">
+        <label for="numero_page">Numéro de la page </label>
+        <input type="text" id="numero_page" name="numero_page" required
+        minlength="4" maxlength="8" size="10">
+        <button>
+            Valider
+        </button>
+
+
+        <?php include('tableur.php'); ?>
+
+    </form>
+</body>
+</html>
